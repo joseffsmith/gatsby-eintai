6e18b8ab9df43be6c2ee562e3af5f0b3b7a97279
joseffsmith <joe@rapidtender.com>
2019-04-21 15:23:19 +0000

    working model, installing material ui plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import styled, { css, keyframes } from 'styled-components';
 
 import Header from "./header"
 import "./layout.css"
+
+const color_grey = "#efefef"
+const color_red = "#a12a2a"
+const color_red_hover = "#ce4141"
+const color_white = "#f9f9f9"
+const color_dark_green = "#0e3f00"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,26 +25,35 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <PageNavbar>
+          <SiteTitle>
+            {data.site.siteMetadata.title}
+          </SiteTitle>
+        </PageNavbar>
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
+            by
+            <a href="https://github.com/joseffsmith">Joseff Smith</a>
           </footer>
-        </div>
       </>
     )}
   />
 )
+
+const PageNavbar = styled.div`
+  background-color: ${color_white};
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1rem 2rem 1rem 0px;
+`
+
+const SiteTitle = styled.div`
+  color: ${color_red};
+  font-size: 1.5rem;
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
